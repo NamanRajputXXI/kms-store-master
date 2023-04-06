979fbebf7706dc84aebe7ca3ec9827d2f6db8eaa
abhishekrathi16 <rathiabhishek53@gmail.com>
2023-04-06 22:57:11 +0530

    style changes

--- kms-store-master.js
+++ kms-store-master.js
@@ -9,14 +9,16 @@ function setStoreMaster(el) {
   function mHead1(mE) {
     var mHead = document.createElement("div");
     mHead.style.backgroundColor = "rgba(0,32,96, 1)";
-    mHead.style.width = "98%";
+    mHead.style.width = "100%";
     mHead.style.height = "5.5vh";
     mHead.style.display = "flex";
     mHead.style.alignItems = "center";
     mHead.style.justifyContent = "space-between";
     mHead.style.position = "fixed";
     mHead.style.top = "0px";
-    mHead.style.margin="10px 10px"
+    mHead.style.margin="10px 0px"
+    mHead.style.borderRadius = "10px"
+    // mHead.style.padding = "0 10px"
     const text1 = "Store Master ";
     const text2 = "X";
     var name = document.createElement("div");
@@ -32,6 +34,8 @@ function setStoreMaster(el) {
     mE.appendChild(mHead);
   }
   mHead1(mTbl_div);
+
+  
 }
 
 setStoreMaster(element);
