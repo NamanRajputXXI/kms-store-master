84d46068f7b90ad0b3e4e49e08196dd1654fdda8
Naman Rajput <namanrajput801@gmail.com>
2023-04-06 22:22:42 +0530

    ui: create elements

--- kms-store-master.js
+++ kms-store-master.js
@@ -1,1 +1,28 @@
-console.log("kms store master");
+function setStoreMaster(el) {
+  var mTbl_div = document.createElement("div");
+  mTbl_div.style.width = "100%";
+  mTbl_div.style.height = "100%";
+  el.appendChild(mTbl_div);
+
+  // Header of this screen
+
+  function mHead1(mE) {
+    var mHead = document.createElement("div");
+    mHead.style.backgroundColor = "rgba(167,210,240, 1)";
+    mHead.style.width = "100vw";
+    mHead.style.height = "5.5vh";
+    mHead.style.display = "flex";
+    mHead.style.alignItems = "center";
+    mHead.style.justifyContent = "space-between";
+    mHead.style.position = "fixed";
+    // mHead.style.padding = "0 px 0 30px";
+    mHead.style.top = "0px";
+    mE.appendChild(mHead);
+    const text1 = "Store Master ";
+    const text2 = "Icon";
+    mHead.innerHTML = `<p> ${text1} </p><p> ${text2}  </p>`;
+  }
+  mHead1(mTbl_div);
+}
+
+setStoreMaster(element);
